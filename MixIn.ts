a53670d2e4d9bb3e713c0e60fe1ce47e92afa7d4
dennis96411 <dennis96411@gmx.com>
2018-07-26 01:42:12 -0400

    Revamp new method and add comments

--- MixIn.ts
+++ MixIn.ts
@@ -1,15 +1,47 @@
-type Instance = { constructor: Function; prototype?: never; [Symbol.unscopables]?(): never; [property: string]: any; };
-type Class = { constructor: Function; prototype: object; };
+/**
+ * A normal or abstract class
+ */
+type Class = Function & { prototype: { constructor: Class; }; };
 
-export function mixIn(Base: Instance | Class, Mixins: (Instance | Class | [Class, ...any[]])[]): void {
+/**
+ * An instance of a class; requires runtime constructor check to ensure that it's not a generic object
+ */
+type Instance = {
+    constructor: Class; // Constructor is a normal or abstract class; check against Object at runtime to ensure it's a valid class
+    prototype?: never; // Exclude classes
+    [Symbol.unscopables]?(): never; // Exclude arrays
+    [property: string]: NonNullable<any>; // Exclude other primitives (except null and undefined unless strictNullChecks is true)
+};
+
+/**
+ * Applies mix-in instance and/or class properties to a base instance or class
+ * @param Base The base instance or class to apply the mix-ins to
+ * @param Mixins The mix-in instances and/or classes to apply to the base
+ */
+export function mixIn(Base: Instance | Class, Mixins: (Instance | Class | [Class, ...any[]])[]): typeof Base {
+    // Perform runtime argument type checks that cannot be done at compile time
+    if (!Base || Base.constructor === Object) // Make sure the base is non-null and that it's not just a generic object
+        throw new TypeError("You must pass a valid instance or class for the base argument!");
+    else // Make sure all the mix-ins are valid instances and/or classes
+        for (const [index, Mixin] of Mixins.entries())
+            if (!Mixin || Mixin.constructor === Object) // Make sure the mix-in is non-null and that it's not just a generic object
+                throw new TypeError(`${Mixin} @ Mixins[${index}] is not a valid instance or class!`);
+
+    // Apply mix-ins
     for (const _Mixin of Mixins) {
-        const   Mixin: any = _Mixin instanceof Array ? _Mixin[0] : _Mixin,
-                constructorArguments: any[] = _Mixin instanceof Array ? _Mixin.slice(1) : [],
-                prototypeProperties: {} = Object.getOwnPropertyDescriptors(Mixin.prototype || Mixin.constructor.prototype);
+        const   Mixin: any = _Mixin instanceof Array ? _Mixin[0] : _Mixin, mixinConstructorArguments: any[] = _Mixin instanceof Array ? _Mixin.slice(1) : [],
+                classProperties: PropertyDescriptorMap = Object.getOwnPropertyDescriptors(Mixin.constructor === Function ? Mixin.prototype : Mixin.constructor.prototype),
+                instanceProperties: PropertyDescriptorMap = Object.getOwnPropertyDescriptors(Mixin.constructor === Function ? new Mixin(...mixinConstructorArguments) : Mixin);
+        delete classProperties.constructor; // Do not copy over constructors
 
-        Object.defineProperties(Base.prototype || Base.constructor.prototype, Object.assign(
-            (delete prototypeProperties.constructor) && prototypeProperties, // Mix-in class prototype fields (mainly methods; excluding constructor)
-            Object.getOwnPropertyDescriptors(Mixin.prototype ? new Mixin(...constructorArguments) : Mixin) // Mix-in instance values defined at instantiation time
-        ));
+        // Copy over mix-in properties appropriately
+        if (Base.constructor === Function) // Base is a class
+            Object.defineProperties(Base.prototype, Object.assign(classProperties, instanceProperties)); // Copy both the mix-in's class and instance properties to the base class' prototype
+        else { // Base is an instance
+            Object.defineProperties(Base.constructor.prototype, classProperties); // Copy the mix-in's class properties to the base instance's constructor's/class' prototype
+            Object.defineProperties(Base, instanceProperties); // Copy the mix-in's instance properties to the base instance
+        }
     }
+
+    return Base;
 }
